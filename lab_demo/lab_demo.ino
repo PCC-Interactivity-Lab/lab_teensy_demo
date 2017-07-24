c775b80e36894d5f348e069124e6c7d26566ab05
Nolan Thomas <30419180+foxbatfangs@users.noreply.github.com>
2017-07-24 15:22:37 -0700

    first commit

---
+++ lab_demo/lab_demo.ino
@@ -0,0 +1,32 @@
+
+int sensorPin = 15;
+int sensorReading = 0;
+int sensorScaled = 0;
+int ambient_light = 0;
+int potScaled = 0;
+int potPin = 14;
+int potReading = 0;
+
+void setup(){
+  Serial.begin(9600);
+  ambient_light = analogRead(sensorPin);
+  pinMode(sensorPin, INPUT);
+  pinMode(potPin, INPUT); 
+}
+
+void loop(){
+  sensorReading = analogRead(sensorPin); 
+  sensorReading = constrain(sensorReading, 590, 1024);
+  sensorScaled = map(sensorReading, 500, ambient_light, 0, 127); //500 was low range of the photocell i tested
+  potReading = analogRead(potPin);
+  potScaled = map(potPin, 0, 1023, 0, 127);
+
+
+  usbMIDI.sendControlChange(1, sensorScaled, 1);
+  usbMIDI.sendControlChange(2, potScaled, 1);  
+
+  delay(100); //just here to slow down the output for easier reading
+  Serial.println(potReading);
+  Serial.println(sensorScaled);
+  
+}
